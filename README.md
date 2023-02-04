a4aa426ff9958d53e4829372e0158bee8d16e83a
Joe Bloggs <vapniks@yahoo.com>
2023-02-04 14:24:30 +0000

    Update Readme

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Some extra packages for TXR lisp: http://www.nongnu.org/txr/index.html
   * quickcheck   - a QuickCheck implementation along with other functions & macros for profiling code
   * csv          - functions & datatypes for handling csv data
 
+and a shell script for searching/browsing the txr manual efficiently using [fuzzy finder (fzf)](https://github.com/junegunn/fzf):
+  * browse_txr_manual.sh
+
--- browse_txr_manual.sh
+++ browse_txr_manual.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Browse txr manpage using fzf
+# Browse txr manpage using fzf.
 # Keybindings are shown in the header line.
 # Options are inherited from $FZF_DEFAULT_OPTS, but --header is overwritten unless you have saved it in $FZF_HEADER
 
@@ -15,7 +15,7 @@ if ! [ -r $TXRHEADERSFILE ]; then
     grep '^\s\+[0-9]\{1,2\}\(\.[0-9]\{1,3\}\)\+\s[A-Z]' $TXRMANPAGEFILE > $TXRHEADERSFILE
 fi
 # Change following variable if you make any changes to keybindings (i.e. the --bind options below)
-FZF_HEADER2="RET/M-v=view section,M-V=view summary,M-h=toggle subheaders"
+FZF_HEADER2="RET/M-v=view section,M-V=view summary,M-h=toggle subheaders,M-y=replace query with current selection"
 
 if ! [ -z $FZF_HEADER ]; then
     FZF_HEADER3="${FZF_HEADER}
@@ -30,6 +30,7 @@ PREVIEWCMD2="grep -F -A 10000 {} ${TXRMANPAGEFILE}|sed -n '1n;/^\s\+[0-9]\{1,2\}
 grep '^\s\+[0-9]\+\.[0-9]\+\s' ${TXRHEADERSFILE}|\
     fzf --height=100% --no-sort --preview-window=right:wrap --preview="${PREVIEWCMD1}" \
 	--history=$TXRFZFHISTORY \
+	--bind "alt-y:replace-query" \
 	--bind "alt-v:execute(${PREVIEWCMD1}|less)" \
 	--bind "alt-V:execute(${PREVIEWCMD2}|less)" \
 	--bind "enter:execute(${PREVIEWCMD1}|less)" \
