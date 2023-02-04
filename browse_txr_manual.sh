0396be5b10915f3c9ea4ca80c62806d2bbe22864
Joe Bloggs <vapniks@yahoo.com>
2023-02-04 14:07:09 +0000

    Refactoring and new keybinding for viewing summary of subheaders

--- browse_txr_manual.sh
+++ browse_txr_manual.sh
@@ -3,9 +3,8 @@
 # Browse txr manpage using fzf
 # Keybindings are shown in the header line.
 # Options are inherited from $FZF_DEFAULT_OPTS, but --header is overwritten unless you have saved it in $FZF_HEADER
-# (in which case it's prepended with info about new keybindings).
 
-
+# Locations of cache files
 TXRMANPAGEFILE=~/tmp/txrmanpage
 TXRHEADERSFILE=~/tmp/txrheaders
 TXRFZFHISTORY=~/tmp/txrfzfhistory
@@ -13,15 +12,26 @@ if ! [ -r $TXRMANPAGEFILE ]; then
     man txr > $TXRMANPAGEFILE
 fi
 if ! [ -r $TXRHEADERSFILE ]; then
-    grep '^\s\+[0-9]\+\(\.[0-9]\+\)\+' $TXRMANPAGEFILE > $TXRHEADERSFILE
+    grep '^\s\+[0-9]\{1,2\}\(\.[0-9]\{1,3\}\)\+\s[A-Z]' $TXRMANPAGEFILE > $TXRHEADERSFILE
+fi
+# Change following variable if you make any changes to keybindings (i.e. the --bind options below)
+FZF_HEADER2="RET/M-v=view section,M-V=view summary,M-h=toggle subheaders"
+
+if ! [ -z $FZF_HEADER ]; then
+    FZF_HEADER3="${FZF_HEADER}
+${FZF_HEADER2}"
+else
+    FZF_HEADER3="${FZF_HEADER2}"
 fi
 
-PREVIEWCMD="grep -F -A 10000 {} ${TXRMANPAGEFILE}|sed -n '1,/^\s\+[0-9]\+\.[0-9]\+\s/p'"
-grep '^\s\+[0-9]\+\.[0-9]\+\s' ${TXRHEADERSFILE}|\
-    fzf --height=100% --no-sort --preview-window=right:wrap --preview="${PREVIEWCMD}" \
-	--history=$TXRFZFHISTORY \
-	--bind "alt-v:execute(${PREVIEWCMD}|less)" \
-	--bind "enter:execute(${PREVIEWCMD}|less)" \
-	--bind "alt-h:select-all+reload(grep '^\s\+[0-9]\+\.[0-9]\+\.[0-9]\+' {+f} >/dev/null && grep '^\s\+[0-9]\+\.[0-9]\+\s' ${TXRHEADERSFILE} || cat ${TXRHEADERSFILE})" \
-	--header "RET=view,M-h=toggle subheaders,${FZF_HEADER}"
+PREVIEWCMD1="grep -F -A 10000 {} ${TXRMANPAGEFILE}|sed -n '1,/^\s\+[0-9]\{1,2\}\.[0-9]\{1,3\}\s[A-Z]/p'"
+PREVIEWCMD2="grep -F -A 10000 {} ${TXRMANPAGEFILE}|sed -n '1n;/^\s\+[0-9]\{1,2\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/h;/Description:/{n;n;N;H;x;p};/^\s\+[0-9]\{1,2\}\.[0-9]\{1,3\}\s[A-Z]/q'"
 
+grep '^\s\+[0-9]\+\.[0-9]\+\s' ${TXRHEADERSFILE}|\
+    fzf --height=100% --no-sort --preview-window=right:wrap --preview="${PREVIEWCMD1}" \
+	--history=$TXRFZFHISTORY \
+	--bind "alt-v:execute(${PREVIEWCMD1}|less)" \
+	--bind "alt-V:execute(${PREVIEWCMD2}|less)" \
+	--bind "enter:execute(${PREVIEWCMD1}|less)" \
+	--bind "alt-h:select-all+reload(grep '^\s\+[0-9]\+\.[0-9]\+\.[0-9]\+' {+f} >/dev/null && grep '^\s\+[0-9]\+\.[0-9]\+\s' ${TXRHEADERSFILE} || cat ${TXRHEADERSFILE})" \
+	--header "${FZF_HEADER3}"
